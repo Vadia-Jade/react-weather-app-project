d9b512768fa152c9f4aacb7c26406e2dece4d7cb
Vadia-Jade <missnaturalfuzz4319@gmail.com>
2025-12-15 14:08:41 +0000

    removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Weather App</h1>
       </header>
+      <footer>
+        <p>
+          This Project was Coded by Jade Coster and is open-sourced on {""}
+        </p>
+      </footer>
     </div>
   );
 }
 
-export default App;
+
