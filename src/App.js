4e68f7dca568c9d91ff60fd948fb1c50b0c4bdf9
Vadia-Jade <missnaturalfuzz4319@gmail.com>
2025-12-16 19:59:51 +0000

    Started api  integration

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ export default function App() {
     <div className="App">
       <div className="container">
         <Weather />
+           <footer>
+                <p>
+                  This Project was Coded by Jade Coster and is open-sourced on {""}
+                  <a href="https://github.com/Vadia-Jade/react-weather-app-project" target="_blank" rel="noreferrer">GitHub</a> {""}
+                  and is hosted on {""} 
+                  <a href="https://stalwart-medovik-544a4b.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>
+               </p>
+            </footer>
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState }from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather(){
-    return(
+  const [weatherData, setWeatherData] = useState ({ready:false})
+  function handleResponse(response){
+    setWeatherData({
+      ready:true,
+      temperature: response.data.temperature.current,
+      description: response.data.condition.description,
+      humidity: response.data.temperature.humidity,
+      wind: response.data.wind.speed,
+    });
+    
+  }
+  if ( weatherData.ready){
+      return(
         <div className="Weather">
             <header>
               <form>
@@ -21,34 +34,33 @@ export default function Weather(){
                      <h1>Madrid</h1>
                      <ul>
                         <li>Monday: 15:00</li>
-                        <li>Cloudy</li>
+                        <li>{weatherData.description}</li>
                      </ul>
                    <div className="row mt-3">
                        <div className="col-6">
                           <img src="https://www.gstatic.com/weather/conditions/v1/svg/cloudy_light.svg"
                              alt="Cloudy" 
                             />
-                              <span className="temperature">11</span>
+                              <span className="temperature">{Math.round(weatherData.temperature)}</span>
                                <span className="unit">°C|°F</span>
                         </div>
-                 
                      <div className="col-6">
                          <ul>
-                            <li>Precipitation: 10%</li>
-                           <li>Humidity: 80%</li>
-                           <li>Wind: 5 km/h</li>
+                           <li>Humidity: {weatherData.humidity}%</li>
+                           <li>Wind: {weatherData.wind} km/h</li>
                         </ul>
                     </div>
                  </div>
              </main>
-             <footer>
-                <p>
-                  This Project was Coded by Jade Coster and is open-sourced on {""}
-                  <a href="https://github.com/Vadia-Jade/react-weather-app-project" target="_blank" rel="noreferrer">GitHub</a> {""}
-                  and is hosted on {""} 
-                  <a href="https://stalwart-medovik-544a4b.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>
-               </p>
-            </footer>
         </div>
     );
+  } else {
+  const apiKey = "034407t747af2dfd1c31bo02c7fc4156"
+  let city = "Lisbon"
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+   return "Loading..."
+  }
+   
+  
 }
